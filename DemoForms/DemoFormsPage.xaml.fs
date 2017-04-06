08efa8ca2b7492ebf0adf6068b9145efa247687f
willsam100 <willsam100@gmail.com>
2017-04-06 13:01:07 +1200

    Adding a view model. Take out the types and see what happens..

--- DemoForms/DemoFormsPage.xaml.fs
+++ DemoForms/DemoFormsPage.xaml.fs
@@ -3,50 +3,19 @@
 open Xamarin.Forms
 open Xamarin.Forms.Xaml
 open System.Collections.ObjectModel
-open Model
 
 type DemoViewModel() as this = 
 
-    // The type of the colleciton is inferred based on its usage
-    let collection = ObservableCollection()
+    // A lot of types declared here, are they really needed?
+    let collection:ObservableCollection<string> = ObservableCollection<string>()
 
-    let markTaskAsCompleted x = 
+    let addTask():unit = collection.Add(this.Entry)
 
-        // The F# compiler is strict and will throw a warning if you do not use the result of calling a function/method
-        // Must be explict about this. Send the result to the ignore function to do this. 
-        collection.Remove (Task x) |> ignore
-        x |> completeTask |> Task |> collection.Add
+    let addTask:Command = new Command(addTask)
 
-    let addTask () = 
-        let task = Task (newTask this.Entry)
-
-        // Since DUs and Records implement equaitly, contains will work as we want it. 
-        match collection.Contains task with 
-        | false -> collection.Add task
-        | true -> ()
-
-    let addNote () = 
-        let note = Note this.Entry
-        match collection.Contains note with 
-        | false -> collection.Add note
-        | true -> ()
-
-
-    let addTask = Command addTask
-    let addNote = Command addNote
-
-    member val Entry = "" with get, set
-    member this.Todos = collection
-    member this.AddTask with get() = addTask
-    member this.AddNote with get() = addNote
-
-    // Syntax for overriding setters and getters
-    member this.SelectedItem 
-        with get() = null
-        and set(value) = 
-            match unbox value with 
-            | Task x -> markTaskAsCompleted x
-            | Note _ -> ()
+    member val Entry:string = "" with get, set
+    member this.Todos:ObservableCollection<string> = collection
+    member this.AddTask with get():Command = addTask
 
 
 type DemoFormsPage() as this =
