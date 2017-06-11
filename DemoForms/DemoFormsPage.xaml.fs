62a1476091ae18890ef9a35b39d950af4f90df20
Sam Williams <sam.williams@Ashleys-MacBook-Pro.local>
2017-06-11 14:32:16 +1200

    Rename asll Tasks to Todos

--- DemoForms/DemoFormsPage.xaml.fs
+++ DemoForms/DemoFormsPage.xaml.fs
@@ -9,13 +9,13 @@ type DemoViewModel() as this =
     // A lot of types declared here, are they really needed?
     let collection:ObservableCollection<string> = ObservableCollection<string>()
 
-    let addTask():unit = collection.Add(this.Entry)
+    let addTodo():unit = collection.Add(this.Entry)
 
-    let addTask:Command = new Command(addTask)
+    let addTodo:Command = new Command(addTodo)
 
     member val Entry:string = "" with get, set
     member this.Todos:ObservableCollection<string> = collection
-    member this.AddTask with get():Command = addTask
+    member this.AddTodo with get():Command = addTodo
 
 
 type DemoFormsPage() as this =
